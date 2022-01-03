5944468f582d7b242dd4a1b69ee915409a8f2533
吳俊緯 <wcw850710@gmail.com>
2022-01-03 15:06:03 +0800

    :sparkles: page-title title 改成非必填

--- js/packages/playground/react-vite-spa/src/components/page/page-title.jsx
+++ js/packages/playground/react-vite-spa/src/components/page/page-title.jsx
@@ -6,51 +6,54 @@ import { mergeWords } from '@yoimu/common-lib'
 
 const { Item } = Breadcrumb
 export const PageTitle = ({
-	title,
+	title: ptitle,
 	just = false,
 	back = false,
+	onBack,
 	icon: Icon,
 	className: pclassName,
 	children,
 }) => {
 	const className = useMemo(
-		() => mergeWords('flex-1 bg-gray-100 px-2 ml-2', pclassName && pclassName),
-		[pclassName],
+		() => mergeWords('flex-1 bg-gray-100 px-2', pclassName && pclassName),
+		[],
 	)
 	const history = useHistory()
-	const _onGoBack = () => history.goBack()
+	const title = useMemo(() => ptitle || document.title, [ptitle])
+	const _onGoBack = () => {
+		onBack?.()
+		history.goBack()
+	}
 
 	return (
-		<Typography>
-			<div className="flex items-center mb-2">
+		<Typography className={'mb-2'}>
+			<Breadcrumb className={className}>
+				<Item>
+					<Link className="text-primary" to={'/'}>
+						<HomeOutlined
+							className={'inline-flex items-center fill-current text-primary'}
+						/>
+					</Link>
+				</Item>
+				{just ? (
+					<Item>
+						{Icon ? <Icon className={'inline-flex items-center mr-1'} /> : null}
+						{title}
+					</Item>
+				) : (
+					children
+				)}
+			</Breadcrumb>
+			<div className="flex items-center mt-2">
 				{back ? (
 					<ArrowLeftOutlined
 						className="text-base mr-3 flex items-center"
 						onClick={_onGoBack}
 					/>
 				) : null}
-				<Typography.Title level={3} style={{ marginBottom: 0 }}>
+				<Typography.Title level={3} className={'mb-0'}>
 					{title}
 				</Typography.Title>
-				<Breadcrumb className={className}>
-					<Item>
-						<Link to={'/'}>
-							<HomeOutlined
-								className={'inline-flex items-center fill-current text-primary'}
-							/>
-						</Link>
-					</Item>
-					{just ? (
-						<Item>
-							{Icon ? (
-								<Icon className={'inline-flex items-center mr-1'} />
-							) : null}
-							{title}
-						</Item>
-					) : (
-						children
-					)}
-				</Breadcrumb>
 			</div>
 		</Typography>
 	)
